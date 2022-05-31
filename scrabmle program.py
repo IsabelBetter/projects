d55a2b6d11497c2a66b64ba1962efeea58417760
IsabelBetter <78112444+IsabelBetter@users.noreply.github.com>
2022-05-31 16:38:24 +0100

    Update scrabmle program.py

--- scrabmle program.py
+++ scrabmle program.py
@@ -1,12 +1,10 @@
 import random
 import time
-import colorama
 
 word = input("Please enter the word you would like to crack:   ")
 word = (word.lower())
 random_word_list = []
 # initialise the random word list
-start_time = time.time()
 for _ in word:
     random_word_list.append(chr(96 + random.randint(1, 26)))
 
@@ -21,7 +19,4 @@ for index, letter in enumerate(word):
         time.sleep(0.0001)
 if random_word_list[index] == letter:
     print("cracked word is", word)
-    time.sleep(2000)
-    end_time = time.time()
-    timetaken = (end_time - start_time)
-    print("it took",timetaken, "seconds to crack your word")
+    endProgram = input("Please press any key to close this program")
